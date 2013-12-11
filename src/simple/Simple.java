37a6d47692a48478c95d22ea0d0d4db5c8f86dda
mc13mt49 <mc13mt49@linux-ac73.site>
2013-12-12 00:45:34 +0530

    first commit

---
+++ src/simple/Simple.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple;
+
+/**
+ *
+ * @author mc13mt49
+ */
+public class Simple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
