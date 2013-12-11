d29e353e599da9576701ace25a25e41faeb4a5f1
mc13mt49 <mc13mt49@linux-ac73.site>
2013-12-12 01:01:27 +0530

    till now

--- src/simple/NotePad.java
+++ src/simple/NotePad.java
@@ -6,14 +6,24 @@
 //http://www.dreamincode.net/forums/topic/66176-creating-a-basic-notepad-application/
 package simple;
 
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 /**
  *
  * @author mc13mt49
  */
-public class NotePad {
-    public static void main(String[] args){
+public class NotePad extends JFrame implements ActionListener{
+    
+    private TextArea textArea=new TextArea("",0,0,TextArea.SCROLLBARS_VERTICAL_ONLY);
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /*public static void main(String[] args){
         NotePad n1=new NotePad();
     }
     public NotePad(){
@@ -21,5 +31,7 @@ public class NotePad {
         mi.setSize(200,300);
         mi.setVisible(true);
         mi.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+    }*/
+    
+    
 }
